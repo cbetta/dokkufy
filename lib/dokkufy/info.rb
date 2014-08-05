de9671632bb9d23cb18a4bbfe9c2b1b143c77083
Cristiano Betta <cbetta@gmail.com>
2014-08-05 17:02:01 +0100

    Fixes issues on 14.04

--- lib/dokkufy/info.rb
+++ lib/dokkufy/info.rb
@@ -1,5 +1,5 @@
 module Dokkufy
-  VERSION = "0.1.2"                 unless defined? Dokkufy::VERSION
+  VERSION = "0.1.3"                 unless defined? Dokkufy::VERSION
   NAME = "dokkufy"                  unless defined? Dokkufy::NAME
   DESCRIPTION = "A Dokku toolchain" unless defined? Dokkufy::DESCRIPTION
 end
--- scripts/install_dokku.sh
+++ scripts/install_dokku.sh
@@ -3,7 +3,11 @@ VERSION=$(lsb_release -sr)
 OLD_VERSION="12.04"
 
 if [ "$VERSION" == "$OLD_VERSION" ]; then
+  # required for 12.04
   sudo apt-get install -y python-software-properties
+else
+  # required workaround for bug on 14.04
+  wget -qO- https://raw.github.com/progrium/dokku/$OPTION1/bootstrap.sh | sudo DOKKU_TAG=v0.2.3 bash
 fi
 
 wget -qO- https://raw.github.com/progrium/dokku/$OPTION1/bootstrap.sh | sudo DOKKU_TAG=v0.2.3 bash
