01579597f417bf3aa6768bbc76c32bf73bc525e2
Cristiano Betta <cbetta@gmail.com>
2014-08-17 17:28:24 +0100

    Updating release to 0.1.4

--- lib/dokkufy/info.rb
+++ lib/dokkufy/info.rb
@@ -1,5 +1,5 @@
 module Dokkufy
-  VERSION = "0.1.3"                 unless defined? Dokkufy::VERSION
+  VERSION = "0.1.4"                 unless defined? Dokkufy::VERSION
   NAME = "dokkufy"                  unless defined? Dokkufy::NAME
   DESCRIPTION = "A Dokku toolchain" unless defined? Dokkufy::DESCRIPTION
 end
