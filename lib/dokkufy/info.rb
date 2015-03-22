3775fa6bf7864d4c69808dddb3d489b9af14fb7a
Cristiano Betta <cbetta@gmail.com>
2015-03-22 18:05:22 +0300

    Changes the Plugin listing page
    The Plugins listing page has moved from the Dokku
    GitHub page to http://progrium.viewdocs.io/dokku/plugins.
    This commit updates Dokkufy to use this new location.

--- lib/dokkufy/info.rb
+++ lib/dokkufy/info.rb
@@ -1,5 +1,5 @@
 module Dokkufy
-  VERSION = "0.2.0"                 unless defined? Dokkufy::VERSION
+  VERSION = "0.2.1"                 unless defined? Dokkufy::VERSION
   NAME = "dokkufy"                  unless defined? Dokkufy::NAME
   DESCRIPTION = "Dokku Provisioning Made Easy" unless defined? Dokkufy::DESCRIPTION
 end
