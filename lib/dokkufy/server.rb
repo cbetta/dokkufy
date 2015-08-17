fc042cb67d08105399bf85323d00d995a33e3dac
Cristiano Betta <cbetta@gmail.com>
2015-08-17 12:01:06 +0100

    WIP

--- lib/dokkufy/server.rb
+++ lib/dokkufy/server.rb
@@ -9,13 +9,19 @@ module Dokkufy
     end
 
     def dokkufy(version, domain)
+      setup_root_ssh_key
       ensure_passwordless_sudo
       install_dokku(version)
       configure_vhost(domain)
-      setup_key
+      setup_dokku_ssh_key
     end
 
-    def setup_key
+    def setup_root_ssh_key
+      command = "ssh-copy-id #{username}@#{hostname}"
+      system command
+    end
+
+    def setup_dokku_ssh_key
       user = `echo $USER`
       public_key = 'id_rsa.pub'
       until File.exist?(File.expand_path("~/.ssh/#{public_key}"))
@@ -33,8 +39,9 @@ module Dokkufy
       filename = Dokkufy::Utils.script method_name
       server = "#{username}@#{hostname}"
       `scp #{filename} #{server}:`
-      options = "'OPTION1=#{args[0]} OPTION2=#{args[1]}"
-      system("ssh -t -q #{server} #{options} ./#{method_name}.sh'")
+      options = "'OPTION1=\"#{args[0]}\" OPTION2=\"#{args[1]}\""
+      command = "ssh -t -q #{server} #{options} ./#{method_name}.sh'"
+      system(command)
       `ssh -t -q #{server} 'rm ~/#{method_name}.sh'`
     end
   end
